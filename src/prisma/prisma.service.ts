dc1e5ba5d382a0895be21f6727c1466d3cf16ea5
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-29 05:00:56 +0800

    feat: implement PrismaService for database connection and update dependency configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL as string);
-    super({ adapter });
+    super();
   }
 
   async onModuleInit() {
